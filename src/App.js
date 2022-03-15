7f474737e97bd1be4993233a192688a95d130d95
Henry Derbyshire <henry@htderbyshire.com>
2022-03-15 13:09:38 +0000

    added basic structure to project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import Header from './components/Header.js';
+import Main from './components/Main.js';
+import Sidebar from './components/Sidebar.js';
+
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Main />
+      <Sidebar />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <h1>
+      TodoList
+    </h1>
+  );
+};
+
+export default Header
