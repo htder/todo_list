b10282c93858f19cb31fba9d9a33557a59189030
Henry Derbyshire <henry@htderbyshire.com>
2022-03-18 15:54:53 +0000

    added icons to middle row of todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import '../styles/Todo.css';
+import {
+  MdCheckBoxOutlineBlank,
+  MdOutlineCheckBox,
+  MdKeyboardArrowDown,
+  MdKeyboardArrowUp,
+} from 'react-icons/md';
+import { BsTrash } from 'react-icons/bs';
 
 function Todo({ title, description, dueDate, completed, priority, project }) {
   return (
@@ -8,7 +15,11 @@ function Todo({ title, description, dueDate, completed, priority, project }) {
         <span className="todo-item todo-title">{title}</span>
         <span className="todo-item todo-date">{dueDate}</span>
       </div>
-      <div className="todo-middle-row">middle</div>
+      <div className="todo-middle-row">
+        <MdCheckBoxOutlineBlank />
+        <MdKeyboardArrowDown />
+        <BsTrash />
+      </div>
       <div className="todo-bottom-row">bottom</div>
       {/* <span className="todo-item todo-desc">{description}</span> */}
       {/* <p>Completed: {completed}</p> */}
