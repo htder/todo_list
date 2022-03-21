2c2f727b7786099a5e568de15ca352161a451d01
Henry Derbyshire <henry@htderbyshire.com>
2022-03-21 12:09:43 +0000

    task window displays correct information, when there are no tasks for selected view

--- src/components/TaskWindow.jsx
+++ src/components/TaskWindow.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import Todo from './Todo';
 import '../styles/TaskWindow.css';
+import { BsTrash } from 'react-icons/bs';
+import {
+  MdCheckBoxOutlineBlank,
+  MdOutlineCheckBox,
+  MdKeyboardArrowDown,
+  MdKeyboardArrowUp,
+} from 'react-icons/md';
+import Todo from './Todo';
 
 function TaskWindow({
   tasks,
@@ -26,13 +33,33 @@ function TaskWindow({
       removeTask={removeTask}
       handleEdit={handleEdit}
       page={page}
+      show={show}
     />
   ));
 
+  const noItems = (
+    <div className="todo-container">
+      <h3 className="todo-container-title">{page}</h3>
+      <div className="todo-main-visible">
+        <div className="todo-top-row">
+          <div className="no-tasks">
+            <span>
+              You currently have no tasks
+              {page === 'all tasks' ? '.' : ` for ${page}.`}
+            </span>
+            <span>Create a new task or project from the menu.</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <div className="task-window">
-      {/* <h3 className="task-window-title">{page}</h3> */}
-      <div className="task-window-container">{show && taskElements}</div>
+      <div className="task-window-container">
+        {show && taskElements}
+        {!tasks.length && noItems}
+      </div>
     </div>
   );
 }
